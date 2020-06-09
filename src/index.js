0c53021179e992094bf5f46f7384835b8f778384
Toby Dawson <tobydawson1@gmail.com>
2020-06-09 15:41:21 +0100

    added api call

--- src/index.js
+++ src/index.js
@@ -2,3 +2,19 @@ import React, { UseState, UseEffect } from "react";
 import ReactDOM from "react-dom";
 
 import axios from "axios";
+
+function Reddit() {
+  const [posts, setPosts] = useState([]);
+
+  React.useEffect(() => {
+    axios.get(`https://www.reddit.com/r/reactjs.json`)
+      .then(res => {
+        const newPosts = res.data.data.children
+          .map(obj => obj.data);
+
+          setPosts(newPosts);
+      });
+  }, []);
+
+  
+}
